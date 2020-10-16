6f13a1d549c1fe5f44f5c3567fa13b0563ffdf62
Jari-Matti Mäkelä <jmjm@iki.fi>
2020-10-16 11:57:56 +0300

    Uusi yritys

--- src/main/java/fi/utu/tech/Keppi.java
+++ src/main/java/fi/utu/tech/Keppi.java
@@ -3,8 +3,14 @@ package fi.utu.tech;
 public class Keppi extends Esine {
     private static int id = 1;
 
+    private int keppiId;
+
+    public Keppi() {
+        keppiId = id++;
+    }
+
     @Override
     public String toString() {
-        return "keppi-" + (id++);
+        return "keppi-" + keppiId;
     }
 }
